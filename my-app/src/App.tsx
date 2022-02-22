88eca97f53ab58649c4062dccecbde6fedbc6270
vlutskevich <vlad01309@mail.ru>
2022-02-22 19:52:09 +0300

    init: create react app

---
+++ my-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
+
+
+const App = function () {
+  return (
+    <Router>
+     
+    </Router>
+  );
+};
+
+export default App;
